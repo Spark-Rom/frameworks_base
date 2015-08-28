b7379431603817a1e9a28fc5d232f8a5dc3c3ec7
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    Change-Id: I24968141051805ee8c9bd5819e5f801c0dd78ef3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5880,6 +5880,13 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        @Readable
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
