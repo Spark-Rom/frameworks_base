4b9e1a35ebc3bb6a2f88e2397136bf1c5159cef4
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    base: Hide power menu on secure lockscreen [1/2]
    Adds an option to enable/disable power menu on secure lockscreen.
    
    Change-Id: I6f0e1e5bf436debda5b241ee1040dd433d9a9569

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5754,7 +5754,14 @@ public final class Settings {
          * Whether to display the torch option in the power menu
          * @hide
          */
-	public static final String POWERMENU_TORCH = "powermenu_torch";
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
+         * Disable power menu on secure lock screens
+         *
+         * @hide
+         */
+        public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
