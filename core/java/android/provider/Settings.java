fc7e8823a47bf54c3a2419710cf1044936906071
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow/nougat By: BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6045,7 +6045,16 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_TICKER_TICK_DURATION =
-                "status_bar_ticker_tick_duration";        
+                "status_bar_ticker_tick_duration";
+
+        /**
+         * Whether to enable status and navigation bar color in battery saver m> 
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
