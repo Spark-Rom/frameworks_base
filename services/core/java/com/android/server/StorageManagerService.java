0b08b559f73c96b299c2184333457d853f78b97e
Scott Mertz <scott@cyngn.com>
2016-03-28 15:29:09 -0700

    MountService: ensure VolumeRecord with UUID exists before modifying
    A client (SystemUI) can pass in a fs UUID that doesn't exist in the
    records.  Guard against this invalid input.
    
    Change-Id: I74666c1cdd56a13efa84385b387ccb3a5d68dfc2
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: spkal01 <kalligeross@gmail.com>

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -2416,6 +2416,8 @@ class StorageManagerService extends IStorageManager.Stub
         Objects.requireNonNull(fsUuid);
         synchronized (mLock) {
             final VolumeRecord rec = mRecords.get(fsUuid);
+            if (rec == null)
+                return;
             rec.nickname = nickname;
             mCallbacks.notifyVolumeRecordChanged(rec);
             writeSettingsLocked();
@@ -2429,6 +2431,8 @@ class StorageManagerService extends IStorageManager.Stub
         Objects.requireNonNull(fsUuid);
         synchronized (mLock) {
             final VolumeRecord rec = mRecords.get(fsUuid);
+            if (rec == null)
+                return;
             rec.userFlags = (rec.userFlags & ~mask) | (flags & mask);
             mCallbacks.notifyVolumeRecordChanged(rec);
             writeSettingsLocked();
