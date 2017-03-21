809891603df762378471c60c0c80963243c57ad3
maxwen <max.weninger@gmail.com>
2017-03-21 17:58:33 +0100

    Suspend Actions [1/3]
    ***************************
    **SDB- Adapt for Q
    Ported from JB by @faust93
    Added screen on/off delay feature by @faust93
    
    Change-Id: Ia8d2bed56c883f43051da4699bf5b19760650623

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6091,6 +6091,36 @@ public final class Settings {
         public static final String STOCK_STATUSBAR_IN_HIDE = "stock_statusbar_in_hide";
 
         /**
+         * @hide
+         */
+        public static final String START_SCREEN_STATE_SERVICE = "start_screen_state_service";
+
+        /**
+         * @hide
+         */
+        public static final String SCREEN_STATE_TWOG = "screen_state_twog";
+
+        /**
+         * @hide
+         */
+        public static final String SCREEN_STATE_GPS = "screen_state_gps";
+
+        /**
+         * @hide
+         */
+        public static final String SCREEN_STATE_MOBILE_DATA = "screen_state_mobile_data";
+
+        /**
+         * @hide
+         */
+        public static final String SCREEN_STATE_OFF_DELAY = "screen_state_off_delay";
+
+        /**
+         * @hide
+         */
+        public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
