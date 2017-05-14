1ef7a40ea99776d03d1ff475f37cdc0b318d75e2
Pranav Vashi <neobuddy89@gmail.com>
2017-05-14 08:55:06 +0530

    [SQUASH] Custom logo customizations [1/2]
    * Original placement logic and custom logo patches
      by LorDClockaN and varund7726 respectively.
    * @neobuddy89: Complete rewrite for oreo with darkicon dispatcher.
    
    -AICP'fied by @eyosen:
      * We are AICP after all.
      * Also make naming rom independent.
      * Brought up to Aicp Pie, 10, 11, 12
      * Includes 27dc548f8c483429af93a265492f2e0e7e514112
    	fwb: statusbar logo code cleanup
      * and more code cleanup
    
    Change-Id: I9f4a9331a3867712f69579f764ef2cebd0bbe717
    Signed-off-by: Julian Veit <claymore1298@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -101,6 +101,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private LinearLayout mSystemIconArea;
     private LinearLayout mCustomIconArea;
     private LinearLayout mCenterClockLayout;
+    private View mStatusBarLogo, mStatusBarLogoRight;
     private View mOngoingCallChip;
     private View mNotificationIconAreaInner;
     private View mCenteredIconArea;
@@ -230,6 +231,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mBatteryMeterView = mStatusBar.findViewById(R.id.battery);
         mBatteryMeterView.addCallback(mBatteryMeterViewCallback);
         mOngoingCallChip = mStatusBar.findViewById(R.id.ongoing_call_chip);
+        mStatusBarLogo = mStatusBar.findViewById(R.id.statusbar_logo);
+        mStatusBarLogoRight = mStatusBar.findViewById(R.id.statusbar_logo_right);
         showSystemIconArea(false);
         showClock(false);
         initEmergencyCryptkeeperText();
@@ -446,6 +449,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private void hideSystemIconArea(boolean animate) {
         animateHide(mBatteryBar, animate);
         animateHide(mSystemIconArea, animate);
+        animateHide(mStatusBarLogoRight, animate);
     }
 
     private void showSystemIconArea(boolean animate) {
@@ -454,6 +458,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         int state = mAnimationScheduler.getAnimationState();
         if (state == IDLE || state == SHOWING_PERSISTENT_DOT) {
             animateShow(mSystemIconArea, animate);
+            animateShow(mStatusBarLogoRight, animate);
         }
     }
 
@@ -488,6 +493,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         animateHide(mCenteredIconArea, animate);
         animateHide(mCustomIconArea, animate);
         animateHide(mCenterClockLayout, animate);
+        animateHide(mStatusBarLogo, animate);
     }
 
     public void showNotificationIconArea(boolean animate) {
@@ -495,6 +501,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         animateShow(mCenteredIconArea, animate);
         animateShow(mCustomIconArea, animate);
         animateShow(mCenterClockLayout, animate);
+        animateShow(mStatusBarLogo, animate);
     }
 
     public void hideOperatorName(boolean animate) {
