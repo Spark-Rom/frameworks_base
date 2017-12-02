9d1684d8e67c5348da099001371cb99e9f2ef2fa
maxwen <max.weninger@gmail.com>
2017-12-02 17:12:31 -0500

    Custom button light [1/2]
    - Bring back the shining light into the dark
    
    overlay config
    <bool name="config_button_brightness_support">false</bool>
    <integer name="config_button_brightness_default">128</integer>
    
    *Aex edits*
    - Use brightness as float for custom button lights since lightsmanager accepts/expects float now
    
    Change-Id: Ia5af8b1d3b7a698668d2787e87e66ceccfa57952

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5944,6 +5944,40 @@ public final class Settings {
         @Readable
         public static final String TOAST_ICON = "toast_icon";
 
+
+        /**
+         * Custom button brightness value for manual mode
+         *
+         * @hide
+         */
+        public static final String CUSTOM_BUTTON_BRIGHTNESS = "custom_button_brightness";
+
+        /**
+         * use same value for buttons as for screen (manual and auto mode)
+         *
+         * @hide
+         */
+        public static final String CUSTOM_BUTTON_USE_SCREEN_BRIGHTNESS = "custom_button_use_screen_brightness";
+
+        /**
+         * disable all button brightness (manual and auto mode)
+         *
+         * @hide
+         */
+        public static final String BUTTON_BACKLIGHT_ENABLE = "button_backlight_enable";
+
+        /**
+         * Timeout value for button lights. 0 = disabled
+         * @hide
+         */
+        public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
+
+        /**
+         * @hide
+         */
+        public static final String BUTTON_BACKLIGHT_ON_TOUCH_ONLY = "button_backlight_on_touch_only";
+
+
         /**
          * Bring back old style mobile data indicators.
          * @hide
