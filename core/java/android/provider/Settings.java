d118edbf56efe2e3b661fb736971c8f0d940f8ef
maxwen <max.weninger@gmail.com>
2018-08-16 20:53:46 +0200

    base: Add navigation bar visibility control [1/2]
    Change-Id: Iefc248dffd5d267ad801ee7aee499de1a9f8d476
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5722,7 +5722,14 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * @hide
+         */
+        public static final String FORCE_SHOW_NAVBAR = "navbar_visibility";
+
+        /**
          * User definable value of pulse notification screen brightness
+         * Keys we no longer back up under the current schema, but want to continue to
+         * process when restoring historical backup datasets.
          *
          * @hide
          */
