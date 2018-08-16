e86c2378616a86f88b1beab6f85b19858827e4cb
Surge Raval <Surge1223@gmail.com>
2018-08-16 23:45:32 +0000

    permissions: Remove Pixel Launcher permissions check
    Change-Id: I8c981808bb8a4881843e2e832932de775f145546
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: spkal01 <kalligeross@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5785,8 +5785,6 @@ public class WindowManagerService extends IWindowManager.Stub
 
     @Override
     public void setRecentsVisibility(boolean visible) {
-        mAtmInternal.enforceCallerIsRecentsOrHasPermission(android.Manifest.permission.STATUS_BAR,
-                "setRecentsVisibility()");
         synchronized (mGlobalLock) {
             mPolicy.setRecentsVisibilityLw(visible);
         }
