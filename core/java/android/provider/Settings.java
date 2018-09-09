f378f71c9184a85f8fca219f2ee9e1ec7ab3a84c
faust93 <monumentum@gmail.com>
2018-09-09 21:10:06 +0800

    Add AlarmsBlocker [1/2]
    Made by faust93. Based on WakelockBlocker by maxwen
    
    neobuddy89: Moved to global settings.
    
    Change-Id: Ida3075aa251443463b86b4bbf2c523d036fba65d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17039,6 +17039,18 @@ public final class Settings {
         public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
         /**
+         * List of alarms blocks selected
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_LIST = "alarm_blocking_list";
+
+        /**
+         * Enable blocking alarm
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_ENABLED = "alarm_blocking_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
