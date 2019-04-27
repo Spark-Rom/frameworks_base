f52821368cb5bc9f0ded4949e202139dbd1433d5
Kshitij Gupta <kshitijgm@gmail.com>
2019-04-27 17:33:44 +0530

    base: Introduce RGB accenter [1/2]
    SDB: Adapt for All Normal AOSP Base. Not everyone is comfortable with Dart & Gradle ;)
    AshutoshSundresh: Fix SystemSettingsValidators with ANY_INTEGER_VALIDATOR
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -96,5 +96,6 @@ public class SystemSettings {
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.COLOR_BUCKET_OVERLAY,
         Settings.System.NOTIFICATION_HEADERS,
+        Settings.System.ACCENT_COLOR,
     };
 }
