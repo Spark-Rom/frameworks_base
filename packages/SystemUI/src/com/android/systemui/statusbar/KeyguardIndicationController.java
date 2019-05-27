ac7ea981624ae8d3f98a4b6dd006ad1dd0c50f83
Pranav Vashi <neobuddy89@gmail.com>
2019-05-28 01:03:58 +0530

    Add toggle for charging animation [1/2]
    spezi77: Adapted for SettingsObserver
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -102,6 +102,7 @@ public class KeyguardIndicationController implements StateListener,
     private KeyguardIndicationTextView mTextView;
     private KeyguardIndicationTextView mDisclosure;
     private LottieAnimationView mChargingIndicationView;
+    private boolean mChargingIndication = true;
     private final IBatteryStats mBatteryInfo;
     private final SettableWakeLock mWakeLock;
     private final DockManager mDockManager;
@@ -437,7 +438,7 @@ public class KeyguardIndicationController implements StateListener,
                         .format(mBatteryLevel / 100f);
                 mTextView.switchIndication(percentage);
             }
-            mChargingIndicationView.setVisibility(View.GONE);
+            updateChargingIndication();
             return;
         }
 
@@ -498,8 +499,12 @@ public class KeyguardIndicationController implements StateListener,
         }
     }
 
+    public void updateChargingIndication(boolean visible) {
+        mChargingIndication = visible;
+    }
+
     private void updateChargingIndication() {
-        if (!mDozing && mPowerPluggedIn) {
+        if (mChargingIndication && !mDozing && mPowerPluggedIn) {
             mChargingIndicationView.setVisibility(View.VISIBLE);
             mChargingIndicationView.playAnimation();
         } else {
