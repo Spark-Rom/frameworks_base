d746bf68f4441a934ac1ca3026aeef992b5bc383
DennySPB <dennyspb@gmail.com>
2019-06-18 13:24:29 +0300

    Smart Charging: allow user set resume level [1/2]
    Change-Id: I48bdac2327c13ea377c1d8b675ea95da2e095f02

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6685,6 +6685,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
+
+        /**
+         * @hide
+         */
         public static final String NOTIFICATION_MATERIAL_DISMISS_BGSTYLE = "notification_material_dismiss_bgstyle";
 
         /**
