9e244157a712fcad4947657edc51af69c87c8d93
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-28 15:26:44 +0200

    SystemUI: Allow customizing footer text [1/2]
    Change-Id: I87d5197f37ae86808bafa71d62dae8cf5ca4f8f3

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -86,5 +86,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.DOZE_ON_CHARGE,
         Settings.System.QS_FOOTER_TEXT_SHOW,
+        Settings.System.QS_FOOTER_TEXT_STRING,
     };
 }
