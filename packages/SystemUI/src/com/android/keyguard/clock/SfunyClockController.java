c2d1cfb2ab2a0e333fd689a4c38e705c5368354a
Adhitya Mohan <me@adhityamohan.in>
2020-02-13 17:53:29 +0530

    SystemUI: Add time refresh capabilities for added clock controllers
    Otherwise it causes time to get stuck on ambient display
    
    Change-Id: I55227d7b3232d51d66f6382057f439e43ff1c679
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -178,6 +178,9 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
+        mView.onTimeChanged();
+        mHourClock.refreshTime();
+        mMinuteClock.refreshTime();
     }
 
     @Override
