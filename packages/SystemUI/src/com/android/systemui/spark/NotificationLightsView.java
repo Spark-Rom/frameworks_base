9056529df56fbb2b4d1b0c5784387ecae30ac734
Ali B <abittin@gmail.com>
2020-05-16 17:05:07 +0300

    NotificationLights: add reverse animation direction
    When notifications are repeated more than once, every
    other notification light starts where the previous one
    ends.
    
    Change-Id: I56ec562badcc7dc92e63bd212974763557b83182
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/spark/NotificationLightsView.java
+++ packages/SystemUI/src/com/android/systemui/spark/NotificationLightsView.java
@@ -102,6 +102,9 @@ public class NotificationLightsView extends RelativeLayout {
         int repeats = Settings.System.getIntForUser(resolver,
                 Settings.System.NOTIFICATION_PULSE_REPEATS, 0,
                 UserHandle.USER_CURRENT);
+        boolean directionIsRestart = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_LIGHT_REPEAT_DIRECTION, 0,
+                UserHandle.USER_CURRENT) != 1;
 
         ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
         ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
@@ -111,7 +114,7 @@ public class NotificationLightsView extends RelativeLayout {
         mLightAnimator.setDuration(duration);
         mLightAnimator.setRepeatCount(repeats == 0 ?
                 ValueAnimator.INFINITE : repeats);
-        mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
+        mLightAnimator.setRepeatMode(directionIsRestart ? ValueAnimator.RESTART : ValueAnimator.REVERSE);
         if (repeats != 0) {
             mLightAnimator.addListener(new AnimatorListener() {
                 @Override
