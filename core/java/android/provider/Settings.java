f1b4b749a81432e3518eb03ba84860fceda8caaa
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-09 15:16:28 +0300

    base: Allow hiding notification headers [1/2]
    Gimme back my space, thank you
    
    Change-Id: I5f63a384d2bd2052d4a8a9d49aff1456d743367f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5601,6 +5601,13 @@ public final class Settings {
         public static final String DESKTOP_MODE = "desktop_mode";
 
         /**
+         * Controls whether to show notification headers
+         * @hide
+         */
+        @Readable
+        public static final String NOTIFICATION_HEADERS = "notification_headers";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
