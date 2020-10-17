8756e21a536c06a5b541c14d065b70b30989c9be
Marko Man <darkobas@gmail.com>
2020-10-17 21:43:35 +0200

    SystemUI: day 0 notification light pulse fixes
    use correct default color resource
    stop animator when no longer needed
    
    Change-Id: I6d80245d281dc08a9cf366b7df8668bb977165ef

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4032,7 +4032,7 @@ public final class NotificationPanelViewController extends PanelViewController i
                 if (activeNotif && pulseReasonNotification) {
                     // show the bars if we have to
                     if (pulseLights) {
-                        mPulseLightsView.animateNotification(true);
+                        mPulseLightsView.animateNotification();
                         mPulseLightsView.setVisibility(View.VISIBLE);
                     } else {
                         // bars can still be visible as leftover
