26df72158e93b877c59e360ecd0b9c13988b0358
Genkzsz11 <Genkzsz11@gmail.com>
2020-10-24 21:40:48 +0700

    Add option to center notification headers [1/2] -- Base on https://github.com/TikkiX2/platform_frameworks_base/commit/f1029be4fc004a0378074849b0d89c01c24e5f8f
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -98,5 +98,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_HEADERS,
         Settings.System.ACCENT_COLOR,
         Settings.System.QS_MEDIA_PLAYER,
+        Settings.System.CENTER_NOTIFICATION_HEADERS,
     };
 }
