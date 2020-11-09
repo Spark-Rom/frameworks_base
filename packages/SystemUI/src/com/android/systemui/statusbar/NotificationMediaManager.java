644610a8a625a43759fcf22c04273f09d03a8649
spkal01 <kalligeross@gmail.com>
2020-11-09 18:59:05 +0900

    [SQUASH] Rempve broken commits
    [SQUASH] Revert several commits related to Volume wake/skip track
    
    Revert "ScreenOff skiptrack: support Spotify/kdeconn/... remote sessions"
    
    This reverts commit d0ff89eb3e25dd88189cdb70ee9652d7f14b35af.
    
    Revert "Fix volume rocker skip track on Ambient Display and Lift to Wake"
    
    This reverts commit 2ba4e8424a3c89cb9270518452d055b165549b96.
    
    Revert "Volume wake/skip track: Make it work independently"
    
    This reverts commit 8e7bb250695e2a6d4390f248ea46fef07644564d.
    
    Revert "base: fix double volume events on screen off"
    
    This reverts commit ce77f5f12b73f5139fb963bda8377b38a15be757.
    
    Revert "base: volume key music control [1/2]"
    
    This reverts commit 94b6b1648442510621310951e60c6887604a43c3.
    
    Revert "Allow APK signature scheme v1 for API 30+ apps"
    
    This reverts commit b6fabf56703922625627f395efe1af9f49066b9e.
    
    fixup! SystemUI: Add navbar layout inversion tuning
    
    * Originally set to false by default.
    
    Change-Id: I6159f0232ba2ee9f45d2afbba7d11072d902d9ac
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Revert "fixup! SystemUI: Add navbar layout inversion tuning"
    
    This reverts commit 6006219f1a54c6b16b714e0058b7cbb7c56ac831.
    
    Revert "SystemUI: Revive navbar layout tuning via sysui_nav_bar tunable"
    
    This reverts commit 0af65524a00ce69c26f3d396928bd07e5ceaed93.
    
    Revert "base: tuner: fix navbar layout switch and remove old cruft"
    
    This reverts commit 20306c5f42f5e7ac7a9195de120fad997b2e6024.
    
    Revert "base: tuner: bring back tuner navbar editor from N"
    
    This reverts commit 20a2606682be3ef45e137de493ea90ff4fade1f4.

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -40,7 +40,6 @@ import android.media.session.PlaybackState;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.os.SystemClock;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.provider.DeviceConfig;
@@ -49,7 +48,6 @@ import android.provider.Settings;
 import android.service.notification.NotificationListenerService;
 import android.util.ArraySet;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -866,38 +864,5 @@ public class NotificationMediaManager implements Dumpable {
          */
         default void onPrimaryMetadataOrStateChanged(MediaMetadata metadata,
                 @PlaybackState.State int state) {}
-    }
-
-    private void triggerKeyEvents(int key, MediaController controller, final Handler h) {
-        long when = SystemClock.uptimeMillis();
-        final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, key, 0);
-        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
-        h.post(new Runnable() {
-            @Override
-            public void run() {
-                controller.dispatchMediaButtonEvent(evDown);
-            }
-        });
-        h.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                controller.dispatchMediaButtonEvent(evUp);
-            }
-        }, 20);
-    }
-
-    public void onSkipTrackEvent(int key, final Handler h) {
-        if (mMediaSessionManager != null) {
-            final List<MediaController> sessions
-                    = mMediaSessionManager.getActiveSessionsForUser(
-                    null, UserHandle.USER_ALL);
-            for (MediaController aController : sessions) {
-                if (PlaybackState.STATE_PLAYING ==
-                        getMediaControllerPlaybackState(aController)) {
-                    triggerKeyEvents(key, aController, h);
-                    break;
-                }
-            }
-        }
     }
 }
