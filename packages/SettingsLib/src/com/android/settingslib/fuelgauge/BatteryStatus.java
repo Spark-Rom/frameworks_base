ba1d7ac2ed2c4601384b1f355094e865a55b2cb6
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-12-29 14:05:01 +0530

    BatteryService: Add Turbo power charging support
    Inspired by oneplus dash charging commit
    
    https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Charlie-117: Port to R
    
    Signed-off-by: Charlie-117 <gmoto1179@gmail.com>
    Change-Id: I12784d37062753a306af9fd9c9d5cbba0cc578ea

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -31,6 +31,7 @@ import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
 import static android.os.BatteryManager.EXTRA_WARP_CHARGER;
 import static android.os.BatteryManager.EXTRA_VOOC_CHARGER;
+import static android.os.BatteryManager.EXTRA_TURBO_POWER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -52,6 +53,7 @@ public class BatteryStatus {
     public static final int CHARGING_DASH = 3;
     public static final int CHARGING_WARP = 4;
     public static final int CHARGING_VOOC = 5;
+    public static final int CHARGING_TURBO_POWER = 6;
 
     public final int status;
     public final int level;
@@ -64,13 +66,14 @@ public class BatteryStatus {
     public final boolean dashChargeStatus;
     public final boolean warpChargeStatus;
     public final boolean voocChargeStatus;
+    public final boolean turboPowerStatus;
 
     public final float temperature;
 
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingWattage, boolean dashChargeStatus,
             boolean warpChargeStatus, boolean voocChargeStatus,
-            boolean present,
+            boolean turboPowerStatus, boolean present,
             int maxChargingCurrent, int maxChargingVoltage,
             float temperature) {
 
@@ -84,6 +87,7 @@ public class BatteryStatus {
         this.dashChargeStatus = dashChargeStatus;
         this.warpChargeStatus = warpChargeStatus;
         this.voocChargeStatus = voocChargeStatus;
+        this.turboPowerStatus = turboPowerStatus;
         this.present = present;
         this.temperature = temperature;
     }
@@ -96,6 +100,7 @@ public class BatteryStatus {
         dashChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
         warpChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_WARP_CHARGER, false);
         voocChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_VOOC_CHARGER, false);
+        turboPowerStatus = batteryChangedIntent.getBooleanExtra(EXTRA_TURBO_POWER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
 
@@ -202,6 +207,7 @@ public class BatteryStatus {
         return dashChargeStatus ? CHARGING_DASH :
                 warpChargeStatus ? CHARGING_WARP :
                 voocChargeStatus ? CHARGING_VOOC :
+                turboPowerStatus ? CHARGING_TURBO_POWER :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :
