74eb80fdd5099c7f090aaab0566a32e0c26c8c11
spkal01 <kalligeross@gmail.com>
2021-03-03 20:03:33 +0530

    Dont show charging animation if battery is charged

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -585,7 +585,7 @@ public class KeyguardIndicationController implements StateListener,
     }
 
     private void updateChargingIndication() {
-        if (mChargingIndication > 0 && mPowerPluggedIn) {
+        if (mChargingIndication > 0 && !mPowerCharged  && mPowerPluggedIn) {
             mChargingIndicationView.setVisibility(View.VISIBLE);
             mChargingIndicationView.playAnimation();
         } else {
