3556aa6346be99f4279ec6ed03de06d19c9f10ef
spkal01 <kalligeross@gmail.com>
2021-03-06 17:45:52 +0530

    Merge tag 'android-11.0.0_r32' of https://android.googlesource.com/platform/frameworks/base into fire
    Android 11.0.0 Release 32 (RQ2A.210305.006)

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -131,6 +131,8 @@ public class KeyguardIndicationController implements StateListener,
     private boolean mPowerPluggedIn;
     private boolean mPowerPluggedInWired;
     private boolean mPowerCharged;
+    private boolean mBatteryOverheated;
+    private boolean mEnableBatteryDefender;
     private int mChargingSpeed;
     private double mChargingWattage;
     private int mBatteryLevel;
@@ -436,7 +438,7 @@ public class KeyguardIndicationController implements StateListener,
                 } else if (!TextUtils.isEmpty(mAlignmentIndication)) {
                     mTextView.switchIndication(mAlignmentIndication);
                     mTextView.setTextColor(mContext.getColor(R.color.misalignment_text_color));
-                } else if (mPowerPluggedIn) {
+                } else if (mPowerPluggedIn || mEnableBatteryDefender) {
                     String indication = computePowerIndication();
                     if (animate) {
                         animateText(mTextView, indication);
@@ -457,7 +459,7 @@ public class KeyguardIndicationController implements StateListener,
             String trustManagedIndication = getTrustManagedIndication();
 
             String powerIndication = null;
-            if (mPowerPluggedIn) {
+            if (mPowerPluggedIn || mEnableBatteryDefender) {
                 powerIndication = computePowerIndication();
             }
 
@@ -480,7 +482,7 @@ public class KeyguardIndicationController implements StateListener,
             } else if (!TextUtils.isEmpty(mAlignmentIndication)) {
                 mTextView.switchIndication(mAlignmentIndication);
                 isError = true;
-            } else if (mPowerPluggedIn) {
+            } else if (mPowerPluggedIn || mEnableBatteryDefender) {
                 if (DEBUG_CHARGING_SPEED) {
                     powerIndication += ",  " + (mChargingWattage / 1000) + " mW";
                 }
@@ -649,8 +651,15 @@ public class KeyguardIndicationController implements StateListener,
             return mContext.getResources().getString(R.string.keyguard_charged);
         }
 
-        final boolean hasChargingTime = mChargingTimeRemaining > 0;
         int chargingId;
+        String percentage = NumberFormat.getPercentInstance().format(mBatteryLevel / 100f);
+
+        if (mBatteryOverheated) {
+            chargingId = R.string.keyguard_plugged_in_charging_limited;
+            return mContext.getResources().getString(chargingId, percentage);
+        }
+
+        final boolean hasChargingTime = mChargingTimeRemaining > 0;
         if (mPowerPluggedInWired) {
             switch (mChargingSpeed) {
                 case BatteryStatus.CHARGING_FAST:
@@ -719,8 +728,6 @@ public class KeyguardIndicationController implements StateListener,
             }
         }
 
-        String percentage = NumberFormat.getPercentInstance()
-                .format(mBatteryLevel / 100f);
         if (hasChargingTime) {
             // We now have battery percentage in these strings and it's expected that all
             // locales will also have it in the future. For now, we still have to support the old
@@ -857,6 +864,8 @@ public class KeyguardIndicationController implements StateListener,
             mChargingSpeed = status.getChargingSpeed(mContext);
             mTemperature = status.temperature;
             mBatteryLevel = status.level;
+            mBatteryOverheated = status.isOverheated();
+            mEnableBatteryDefender = mBatteryOverheated && status.isPluggedIn();
             try {
                 mChargingTimeRemaining = mPowerPluggedIn
                         ? mBatteryInfo.computeChargeTimeRemaining() : -1;
