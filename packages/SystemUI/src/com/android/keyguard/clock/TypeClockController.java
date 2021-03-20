88d9f561a137b8e5100079e0d39a7f9c14d29d39
spkal01 <kalligeross@gmail.com>
2021-03-20 15:11:41 +0530

    Fix type clock crashing when being selected

--- packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -24,14 +24,11 @@ import android.graphics.Paint.Style;
 import android.util.MathUtils;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.provider.Settings;
-import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
-import com.android.internal.util.spark.SparkUtils;
 
 import java.util.TimeZone;
 
@@ -75,8 +72,6 @@ public class TypeClockController implements ClockPlugin {
     private View mView;
     private TypographicClock mTypeClock;
 
-    private Context mContext;
-
     /**
      * Small clock shown on lock screen above stack scroller.
      */
@@ -95,7 +90,7 @@ public class TypeClockController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     TypeClockController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor, Context context) {
+            SysuiColorExtractor colorExtractor) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
@@ -188,13 +183,8 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
-            mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-        } else {
-            mTypeClock.setTextColor(color);
-            mLockClock.setTextColor(color);
-        }
+        mTypeClock.setTextColor(color);
+        mLockClock.setTextColor(color);
     }
 
     @Override
@@ -203,12 +193,8 @@ public class TypeClockController implements ClockPlugin {
             return;
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
-        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
-            mTypeClock.setClockColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-        } else {
-            mTypeClock.setClockColor(color);
-            mLockClock.setClockColor(color);
-        }
+        mTypeClock.setTextColor(color);
+        mLockClock.setTextColor(color);
     }
 
     @Override
@@ -223,6 +209,7 @@ public class TypeClockController implements ClockPlugin {
         if (mDarkController != null) {
             mDarkController.setDarkAmount(darkAmount);
         }
+        mClockPosition.setDarkAmount(darkAmount);
     }
 
     @Override
