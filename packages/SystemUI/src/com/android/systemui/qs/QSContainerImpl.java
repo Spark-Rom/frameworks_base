496c23bd3a4e452d11881e22ba2a7a0f014ed86c
spkal01 <kalligeross@gmail.com>
2021-04-18 08:38:29 +0530

    Revert "SystemUI: Remove black QS status bar background"
    This reverts commit fd37ffeca4d06ac0033eb95cbd31da061c8e9f4d.

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -90,6 +90,8 @@ public class QSContainerImpl extends FrameLayout implements ColorExtractor.OnCol
     private View mQSPanelContainer;
 
     private View mBackground;
+    private View mBackgroundGradient;
+    private View mStatusBarBackground;
 
     private int mSideMargins;
     private boolean mQsDisabled;
@@ -128,6 +130,8 @@ public class QSContainerImpl extends FrameLayout implements ColorExtractor.OnCol
         mQSCustomizer = findViewById(R.id.qs_customize);
         mDragHandle = findViewById(R.id.qs_drag_handle_view);
         mBackground = findViewById(R.id.quick_settings_background);
+        mStatusBarBackground = findViewById(R.id.quick_settings_status_bar_background);
+        mBackgroundGradient = findViewById(R.id.quick_settings_gradient_view);
         updateResources();
         mHeader.getHeaderQsPanel().setMediaVisibilityChangedListener((visible) -> {
             if (mHeader.getHeaderQsPanel().isShown()) {
@@ -225,6 +229,7 @@ public class QSContainerImpl extends FrameLayout implements ColorExtractor.OnCol
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        setBackgroundGradientVisibility(newConfig);
         updateResources();
         mSizePoint.set(0, 0); // Will be retrieved on next measure pass.
     }
@@ -329,6 +334,7 @@ public class QSContainerImpl extends FrameLayout implements ColorExtractor.OnCol
         final boolean disabled = (state2 & DISABLE2_QUICK_SETTINGS) != 0;
         if (disabled == mQsDisabled) return;
         mQsDisabled = disabled;
+        setBackgroundGradientVisibility(getResources().getConfiguration());
         mBackground.setVisibility(mQsDisabled ? View.GONE : View.VISIBLE);
     }
 
@@ -396,6 +402,16 @@ public class QSContainerImpl extends FrameLayout implements ColorExtractor.OnCol
                 + mHeader.getHeight();
     }
 
+    private void setBackgroundGradientVisibility(Configuration newConfig) {
+        if (newConfig.orientation == ORIENTATION_LANDSCAPE) {
+            mBackgroundGradient.setVisibility(View.INVISIBLE);
+            mStatusBarBackground.setVisibility(View.INVISIBLE);
+        } else {
+            mBackgroundGradient.setVisibility(mQsDisabled ? View.INVISIBLE : View.VISIBLE);
+            mStatusBarBackground.setVisibility(View.VISIBLE);
+        }
+    }
+
     public void setExpansion(float expansion) {
         mQsExpansion = expansion;
         mDragHandle.setAlpha(1.0f - expansion);
@@ -405,7 +421,8 @@ public class QSContainerImpl extends FrameLayout implements ColorExtractor.OnCol
     private void updatePaddingsAndMargins() {
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
-            if (view == mQSCustomizer) {
+            if (view == mStatusBarBackground || view == mBackgroundGradient
+                    || view == mQSCustomizer) {
                 // Some views are always full width
                 continue;
             }
