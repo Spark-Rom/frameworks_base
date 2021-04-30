0223c712333c305b970cbf0f79e1ff47dc7f80ab
El Dainosor <eldainosor@gmail.com>
2021-04-30 13:37:25 +0200

    Clocks: Add Java IDE and Type (Alt) lockscreen clocks
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -169,6 +169,9 @@ public final class ClockManager {
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ClockertinoClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OctaviDigitalClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new IDEJavaClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new TypeAltClockController(res, layoutInflater, colorExtractor));
+
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
