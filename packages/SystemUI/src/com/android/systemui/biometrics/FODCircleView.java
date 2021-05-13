37909fcc6535e939104af209bdc57825cbfc9c6c
00day0 <therandomuser11@gmail.com>
2021-05-13 22:00:21 +0530

    SystemUI: Implement FOD icon animation (1/2)
    Reverse engineered from ExtendedUI-2.1-GAPPS-R-OFFICIAL-cheeseburger-20210102-0435
    
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I0a1c7ded36dc765ba075af5c62608b1eefd284b0

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -82,6 +82,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     private final WindowManager mWindowManager;
 
     private IFingerprintInscreen mFingerprintInscreenDaemon;
+    private FODIconView mFODIcon;
 
     private int mColorBackground;
     private int mDreamingOffsetY;
@@ -204,6 +205,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
                 mFODAnimation.setAnimationKeyguard(mIsKeyguard);
             }
             handlePocketManagerCallback(showing);
+            if (mFODIcon != null) {
+                mFODIcon.setIsKeyguard(mIsKeyguard);
+            }
         }
 
         @Override
@@ -323,6 +327,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
             mFODAnimation = new FODAnimation(mContext, mWindowManager, mPositionX, mPositionY);
         }
 
+        mFODIcon = new FODIconView(mContext, mSize, mPositionX, mPositionY);
         mNavigationBarSize = res.getDimensionPixelSize(R.dimen.navigation_bar_size);
 
         mDreamingMaxOffset = (int) (mSize * 0.1f);
@@ -526,7 +531,6 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     public void hideCircle() {
         mIsCircleShowing = false;
 
-        setImageResource(ICON_STYLES[mSelectedIcon]);
         invalidate();
 
         dispatchRelease();
@@ -549,6 +553,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
             return;
         }
 
+        mFODIcon.show();
         updatePosition();
 
         dispatchShow();
@@ -556,6 +561,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     }
 
     public void hide() {
+        mFODIcon.hide();
         setVisibility(View.GONE);
         hideCircle();
         dispatchHide();
@@ -618,10 +624,14 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         }
 
         mWindowManager.updateViewLayout(this, mParams);
+        FODIconView fODIconView = this.mFODIcon;
 
         if (mPressedView.getParent() != null) {
             mWindowManager.updateViewLayout(mPressedView, mPressedParams);
         }
+
+        WindowManager.LayoutParams layoutParams3 = this.mParams;
+        fODIconView.updatePosition(layoutParams3.x, layoutParams3.y);
     }
 
     private void setDim(boolean dim) {
