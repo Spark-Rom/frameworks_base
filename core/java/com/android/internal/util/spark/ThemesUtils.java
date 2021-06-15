f577383ea990267158292213319f080787157a41
SagarMakhar <sagarmakhar@gmail.com>
2021-06-15 08:17:16 +0000

    ThemesUtils: Add function to return clockfaces
    Change-Id: I025662c04dc42d93f2b132a7997209474ac8b0d0
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/com/android/internal/util/spark/ThemesUtils.java
+++ core/java/com/android/internal/util/spark/ThemesUtils.java
@@ -16,8 +16,14 @@
 
 package com.android.internal.util.spark;
 
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import static android.os.UserHandle.USER_SYSTEM;
-
+import android.content.ContentResolver;
+import android.content.pm.ProviderInfo;
+import android.database.Cursor;
+import com.android.internal.util.spark.clock.ClockFace;
+import android.net.Uri;
 import android.app.UiModeManager;
 import android.content.Context;
 import android.content.om.IOverlayManager;
@@ -27,9 +33,18 @@ import android.os.ServiceManager;
 import android.provider.Settings;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class ThemesUtils {
 
+    private Context mContext;
     public static final String TAG = "ThemesUtils";
+
+    public ThemesUtils(Context context) {
+        mContext = context;
+    }
 
     // QS Tile Styles
     public static final String[] QS_TILE_THEMES = {
@@ -153,5 +168,38 @@ public class ThemesUtils {
                 e.printStackTrace();
             }
         }
+    }
+
+    public List<ClockFace> getClocks() {
+        ProviderInfo providerInfo = mContext.getPackageManager().resolveContentProvider("com.android.keyguard.clock",
+                        PackageManager.MATCH_SYSTEM_ONLY);
+
+        Uri optionsUri = new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_CONTENT)
+                .authority(providerInfo.authority)
+                .appendPath("list_options")
+                .build();
+
+        ContentResolver resolver = mContext.getContentResolver();
+        List<ClockFace> clocks = new ArrayList<>();
+        try (Cursor c = resolver.query(optionsUri, null, null, null, null)) {
+            while(c.moveToNext()) {
+                String id = c.getString(c.getColumnIndex("id"));
+                String title = c.getString(c.getColumnIndex("title"));
+                String previewUri = c.getString(c.getColumnIndex("preview"));
+                Uri preview = Uri.parse(previewUri);
+                String thumbnailUri = c.getString(c.getColumnIndex("thumbnail"));
+                Uri thumbnail = Uri.parse(thumbnailUri);
+
+                ClockFace.Builder builder = new ClockFace.Builder();
+                builder.setId(id).setTitle(title).setPreview(preview).setThumbnail(thumbnail);
+                clocks.add(builder.build());
+            }
+        } catch (Exception e) {
+            clocks = null;
+        } finally {
+            // Do Nothing
+        }
+        return clocks;
     }
 }
