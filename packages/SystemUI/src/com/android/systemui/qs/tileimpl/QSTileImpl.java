4bb4a44e4a1afe4f4dfbbc7c8f3920244f18dc6d
spkal01 <kalligeross@gmail.com>
2021-06-16 20:09:39 +0530

    Revert "setCurrentState(DESTROYED) called from main thread"
    This reverts commit 7fd8bc56066f97d03d014a346f51a590b7e64483.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -14,7 +14,6 @@
 
 package com.android.systemui.qs.tileimpl;
 
-import static androidx.lifecycle.Lifecycle.State.CREATED;
 import static androidx.lifecycle.Lifecycle.State.DESTROYED;
 import static androidx.lifecycle.Lifecycle.State.RESUMED;
 import static androidx.lifecycle.Lifecycle.State.STARTED;
@@ -173,7 +172,6 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         mQSLogger = host.getQSLogger();
         mUiEventLogger = host.getUiEventLogger();
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
-        mUiHandler.post(() -> mLifecycle.setCurrentState(CREATED));
     }
 
     protected final void resetStates() {
@@ -469,9 +467,6 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 if (DEBUG) Log.d(TAG, "handleSetListening true");
                 handleSetListening(listening);
                 mUiHandler.post(() -> {
-                    // This tile has been destroyed, the state should not change anymore and we
-                    // should not refresh it anymore.
-                    if (mLifecycle.getCurrentState().equals(DESTROYED)) return;
                     mLifecycle.setCurrentState(RESUMED);
                     refreshState(); // Ensure we get at least one refresh after listening.
                 });
@@ -480,11 +475,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             if (mListeners.remove(listener) && mListeners.size() == 0) {
                 if (DEBUG) Log.d(TAG, "handleSetListening false");
                 handleSetListening(listening);
-                mUiHandler.post(() -> {
-                    // This tile has been destroyed, the state should not change anymore.
-                    if (mLifecycle.getCurrentState().equals(DESTROYED)) return;
-                    mLifecycle.setCurrentState(STARTED);
-                });
+                mUiHandler.post(() -> mLifecycle.setCurrentState(STARTED));
             }
         }
         updateIsFullQs();
@@ -511,14 +502,11 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         mQSLogger.logTileDestroyed(mTileSpec, "Handle destroy");
         if (mListeners.size() != 0) {
             handleSetListening(false);
-            mListeners.clear();
         }
         mCallbacks.clear();
         mHandler.removeCallbacksAndMessages(null);
         // This will force it to be removed from all controllers that may have it registered.
-        mUiHandler.post(() -> {
-            mLifecycle.setCurrentState(DESTROYED);
-        });
+        mLifecycle.setCurrentState(DESTROYED);
     }
 
     protected void checkIfRestrictionEnforcedByAdminOnly(State state, String userRestriction) {
