Octavi-OS/platform_frameworks_base

Keyguard: Make clock widgets great again
@ZeNiXxX
* allow setting accent color for Minutes and hours on every digital clock
* also allow setting accent color for custom clocks

packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -261,7 +261,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
         // Initialize plugin parameters.
         mClockPlugin = plugin;
         mClockPlugin.setStyle(getPaint().getStyle());
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenClockHourAccentColor(mContext)) {
             mClockPlugin.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mClockPlugin.setTextColor(getCurrentTextColor());
@@ -301,7 +301,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
      * It will also update plugin setTextColor if plugin is connected.
      */
     public void setTextColor(int color) {
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenClockHourAccentColor(mContext)) {
             mClockView.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mClockViewBold.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             if (mClockPlugin != null) {

packages/SystemUI/src/com/android/keyguard/clock/AnalogClockController.java
@@ -186,7 +186,7 @@ public class AnalogClockController implements ClockPlugin {
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
 
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {

packages/SystemUI/src/com/android/keyguard/clock/DefaultBoldClockController.java
@@ -95,8 +95,21 @@ public class DefaultBoldClockController implements ClockPlugin {
         mView = (ClockLayout) mLayoutInflater
                 .inflate(R.layout.digital_clock_custom, null);
         mClock = mView.findViewById(R.id.clock);
-        mClock.setFormat12Hour(Html.fromHtml("<strong>h</strong>:mm"));
-        mClock.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:mm"));
+        int mAccentColor = mContext.getResources().getColor(R.color.lockscreen_clock_accent_color);
+
+        if(OctaviUtils.useLockscreenClockMinuteAccentColor(mContext) && OctaviUtils.useLockscreenClockHourAccentColor(mContext)) {
+             mClock.setFormat12Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">h</font></strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+             mClock.setFormat24Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">kk</font></strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+        } else if(OctaviUtils.useLockscreenClockHourAccentColor(mContext)) {
+             mClock.setFormat12Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">h</font></strong>:<strong>mm</strong>"));
+             mClock.setFormat24Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">kk</font></strong>:<strong>mm</strong>"));
+        } else if(OctaviUtils.useLockscreenClockMinuteAccentColor(mContext)) {
+             mClock.setFormat12Hour(Html.fromHtml("<strong>h</strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+             mClock.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+        } else {
+            mClock.setFormat12Hour(Html.fromHtml("<strong>h</strong>:<strong>mm</strong>"));
+            mClock.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:<strong>mm</strong>"));
+        }
     }
 
     @Override
@@ -163,11 +176,6 @@ public class DefaultBoldClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
-            mClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-        } else {
-            mClock.setTextColor(color);
-        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/DotClockController.java
@@ -183,7 +183,7 @@ public class DotClockController implements ClockPlugin {
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
         final int secondary = mPalette.getSecondaryColor();
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mLockClock.setTextColor(secondary);
@@ -213,6 +213,6 @@ public class DotClockController implements ClockPlugin {
 
     @Override
     public boolean shouldShowStatusArea() {
-        return true;
+        return false;
     }
 }

packages/SystemUI/src/com/android/keyguard/clock/SpectrumClockController.java
@@ -183,7 +183,7 @@ public class SpectrumClockController implements ClockPlugin {
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
         final int secondary = mPalette.getSecondaryColor();
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mLockClock.setTextColor(secondary);

packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -189,7 +189,7 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
@@ -204,7 +204,7 @@ public class TypeClockController implements ClockPlugin {
             return;
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mTypeClock.setClockColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mTypeClock.setClockColor(color);

Spark-Rom/frameworks_base

packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -265,7 +265,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
         // Initialize plugin parameters.
         mClockPlugin = plugin;
         mClockPlugin.setStyle(getPaint().getStyle());
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenClockHourAccentColor(mContext)) {
             mClockPlugin.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mClockPlugin.setTextColor(getCurrentTextColor());
@@ -305,7 +305,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
      * It will also update plugin setTextColor if plugin is connected.
      */
     public void setTextColor(int color) {
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenClockHourAccentColor(mContext)) {
             mClockView.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mClockViewBold.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             if (mClockPlugin != null) {

packages/SystemUI/src/com/android/keyguard/clock/AnalogClockController.java
@@ -186,7 +186,7 @@ public class AnalogClockController implements ClockPlugin {
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
 
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {

packages/SystemUI/src/com/android/keyguard/clock/DefaultBoldClockController.java
@@ -95,8 +95,21 @@ public class DefaultBoldClockController implements ClockPlugin {
         mView = (ClockLayout) mLayoutInflater
                 .inflate(R.layout.digital_clock_custom, null);
         mClock = mView.findViewById(R.id.clock);
-        mClock.setFormat12Hour(Html.fromHtml("<strong>h</strong>:mm"));
-        mClock.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:mm"));
+        int mAccentColor = mContext.getResources().getColor(R.color.lockscreen_clock_accent_color);
+
+        if(SparkUtils.useLockscreenClockMinuteAccentColor(mContext) && SparkUtils.useLockscreenClockHourAccentColor(mContext)) {
+             mClock.setFormat12Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">h</font></strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+             mClock.setFormat24Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">kk</font></strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+        } else if(SparkUtils.useLockscreenClockHourAccentColor(mContext)) {
+             mClock.setFormat12Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">h</font></strong>:<strong>mm</strong>"));
+             mClock.setFormat24Hour(Html.fromHtml("<strong><font color=" + mAccentColor + ">kk</font></strong>:<strong>mm</strong>"));
+        } else if(SparkUtils.useLockscreenClockMinuteAccentColor(mContext)) {
+             mClock.setFormat12Hour(Html.fromHtml("<strong>h</strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+             mClock.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:<strong><font color=" + mAccentColor + ">mm</font></strong>"));
+        } else {
+            mClock.setFormat12Hour(Html.fromHtml("<strong>h</strong>:<strong>mm</strong>"));
+            mClock.setFormat24Hour(Html.fromHtml("<strong>kk</strong>:<strong>mm</strong>"));
+        }
     }
 
     @Override
@@ -163,11 +176,6 @@ public class DefaultBoldClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
-            mClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-        } else {
-            mClock.setTextColor(color);
-        }
     }
 
     @Override

packages/SystemUI/src/com/android/keyguard/clock/DotClockController.java
@@ -183,7 +183,7 @@ public class DotClockController implements ClockPlugin {
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
         final int secondary = mPalette.getSecondaryColor();
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mLockClock.setTextColor(secondary);
@@ -213,6 +213,6 @@ public class DotClockController implements ClockPlugin {
 
     @Override
     public boolean shouldShowStatusArea() {
-        return true;
+        return false;
     }
 }

packages/SystemUI/src/com/android/keyguard/clock/SpectrumClockController.java
@@ -183,7 +183,7 @@ public class SpectrumClockController implements ClockPlugin {
     private void updateColor() {
         final int primary = mPalette.getPrimaryColor();
         final int secondary = mPalette.getSecondaryColor();
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mLockClock.setTextColor(secondary);

packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -188,7 +188,7 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
@@ -203,7 +203,7 @@ public class TypeClockController implements ClockPlugin {
             return;
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
-        if(SparkUtils.useLockscreenClockAccentColor(mContext)) {
+        if(SparkUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mTypeClock.setClockColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mTypeClock.setClockColor(color);