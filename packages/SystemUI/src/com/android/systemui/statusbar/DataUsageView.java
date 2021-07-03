a07d3e862dda910a1134f8f29b1b9938304082a3
spkal01 <kalligeross@gmail.com>
2021-07-03 21:24:01 +0530

    Revert "SystemUI: Show carrier name in data usage"
    This reverts commit 9c137751c8dfeddbebc60055cdb80c7ed01cb17c.

--- packages/SystemUI/src/com/android/systemui/statusbar/DataUsageView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/DataUsageView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.os.AsyncTask;
 import android.provider.Settings;
-import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.text.BidiFormatter;
 import android.text.format.Formatter;
@@ -17,7 +16,7 @@ import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
 
-import java.util.List;
+import android.provider.Settings;
 
 public class DataUsageView extends TextView {
 
@@ -57,8 +56,7 @@ public class DataUsageView extends TextView {
                 SubscriptionManager.getDefaultDataSubscriptionId());
         final DataUsageController.DataUsageInfo info = showDailyDataUsage ? mobileDataController.getDailyDataUsageInfo()
                 : mobileDataController.getDataUsageInfo();
-        formatedinfo = getSlotCarrierName() + ": " + formatDataUsage(info.usageLevel) + " "
-                + mContext.getResources().getString(R.string.usage_data);
+        formatedinfo = formatDataUsage(info.usageLevel) + " " + mContext.getResources().getString(R.string.usage_data);
         shouldUpdateDataTextView = true;
     }
 
@@ -67,22 +65,5 @@ public class DataUsageView extends TextView {
                 Formatter.FLAG_IEC_UNITS);
         return BidiFormatter.getInstance().unicodeWrap(mContext.getString(
                 com.android.internal.R.string.fileSizeSuffix, res.value, res.units));
-    }
-
-    private String getSlotCarrierName() {
-        CharSequence result = "";
-        SubscriptionManager subManager = mContext.getSystemService(SubscriptionManager.class);
-        int subId = subManager.getDefaultDataSubscriptionId();
-        List<SubscriptionInfo> subInfoList =
-                subManager.getActiveSubscriptionInfoList(true);
-        if (subInfoList != null) {
-            for (SubscriptionInfo subInfo : subInfoList) {
-                if (subId == subInfo.getSubscriptionId()) {
-                    result = subInfo.getDisplayName();
-                    break;
-                }
-            }
-        }
-        return result.toString();
     }
 }
