6a844ecd978a366297c7fbc768e72c57d88c3a42
jhonboy121 <alfredmathew05@gmail.com>
2021-09-06 01:21:34 +0530

    base: rework FontService
    * Fonts can now be set by directly choosing the ttf files
    * font config xml is auto generated, which might be extended for custom font config support at a later time
    * added callbacks to notify when fonts are added and removed as addFonts and removeFonts are asynchronous
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4976,16 +4976,16 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
-         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         * Stores information about current font as a FontInfo object converted to string with "|" as a delimiter
          * @hide
          */
         public static final String FONT_INFO = "font_info";
 
         /**
-         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         * Hold list of installed user selected. Uses "|" as a delimiter
          * @hide
          */
-        public static final String FONT_PACKAGES = "font_packages";
+        public static final String FONT_LIST = "font_list";
 
         /**
          * Whether to blink flashlight for incoming calls
