b0d3ab6a5a58975d8c2e266eab6e83d16bdd7c31
DennySPb <dennyspb@gmail.com>
2021-09-10 13:59:31 +0300

    GamingMode: Option for disable menu overlay [1/3]
        * I found a game (Oddmar) that does not work with the touchscreen
          if the overlay is enabled. There may be others.
    
    Change-Id: Ic3de104aaf85be166218c98868135155e234124a
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6579,8 +6579,13 @@ public final class Settings {
         public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
 
         /**
+          * @hide
+          */
+        public static final String GAMING_MODE_USE_OVERLAY_MENU = "gaming_mode_use_overlay_menu";
+
+        /**
          * Whether to use Wallpaper Color On S Clocks
-         * @hide
+          * @hide
          */
         public static final String LS_WALLPAPER_COLOR = "ls_wallpaper_color";
 
