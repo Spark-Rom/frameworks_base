27b4a9e3b667a872dfbaab57ea801a89c57ceec5
spkal01 <kalligeross@gmail.com>
2021-11-22 01:05:40 +0900

    Revert "base: Add option to cycle through ringer modes [1/2]"

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10637,7 +10637,7 @@ public final class Settings {
 
         /**
          * What behavior should be invoked when the volume hush gesture is triggered
-         * One of VOLUME_HUSH_OFF, VOLUME_HUSH_VIBRATE, VOLUME_HUSH_MUTE, VOLUME_HUSH_CYCLE.
+         * One of VOLUME_HUSH_OFF, VOLUME_HUSH_VIBRATE, VOLUME_HUSH_MUTE.
          *
          * @hide
          */
@@ -10654,8 +10654,6 @@ public final class Settings {
         /** @hide */
         @SystemApi
         public static final int VOLUME_HUSH_MUTE = 2;
-        /** @hide */
-        public static final int VOLUME_HUSH_CYCLE = 3;
 
         /**
          * The number of times (integer) the user has manually enabled battery saver.
