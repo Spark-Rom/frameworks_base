a37cf7c1c0b3ac4aa1cf55e10e0dc8f89f07b9a8
jhonboy121 <alfredmathew05@gmail.com>
2021-12-05 21:07:18 +0530

    NotificationPanelViewController: set FLAG_IMMUTABLE for edge lighting
    timer PendingIntent's
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -5595,7 +5595,7 @@ public class NotificationPanelViewController extends PanelViewController impleme
         Intent intent = new Intent(CANCEL_NOTIFICATION_PULSE_ACTION);
         intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
         PendingIntent sender = PendingIntent.getBroadcast(mView.getContext(),
-                0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                0, intent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, when, sender);
     }
 
@@ -5606,7 +5606,7 @@ public class NotificationPanelViewController extends PanelViewController impleme
         AlarmManager alarmManager = (AlarmManager) mView.getContext().getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(CANCEL_NOTIFICATION_PULSE_ACTION);
         PendingIntent sender = PendingIntent.getBroadcast(mView.getContext(),
-                0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                0, intent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         alarmManager.cancel(sender);
     }
 
