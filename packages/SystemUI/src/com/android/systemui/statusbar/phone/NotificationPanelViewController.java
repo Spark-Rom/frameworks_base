a557eb82dbd6ba91b196332ac71f8793508f8c16
jhonboy121 <alfredmathew05@gmail.com>
2021-12-05 21:07:18 +0530

    SystemUI: NotificationPanelViewController: set FLAG_IMMUTABLE for notification pulse timer PendingIntent's
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I223449d5ae460433dceb6ad8f3f64c96d89647e0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4988,7 +4988,7 @@ public class NotificationPanelViewController extends PanelViewController {
         Intent intent = new Intent(CANCEL_NOTIFICATION_PULSE_ACTION);
         intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
         PendingIntent sender = PendingIntent.getBroadcast(mView.getContext(),
-                0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                0, intent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, when, sender);
     }
 
@@ -4999,7 +4999,7 @@ public class NotificationPanelViewController extends PanelViewController {
         AlarmManager alarmManager = (AlarmManager) mView.getContext().getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(CANCEL_NOTIFICATION_PULSE_ACTION);
         PendingIntent sender = PendingIntent.getBroadcast(mView.getContext(),
-                0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                0, intent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         alarmManager.cancel(sender);
     }
 }
