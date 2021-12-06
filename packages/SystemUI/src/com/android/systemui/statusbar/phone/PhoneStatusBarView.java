25e5338c80ee206f4adae01dc334f7c1bd46a3aa
jhonboy121 <alfredmathew05@gmail.com>
2021-12-07 01:46:36 +0530

    Merge tag 'android-12.0.0_r16' of https://android.googlesource.com/platform/frameworks/base into spark
    Android 12.0.0 release 16
    
    Change-Id: Iedaf239f46211e2a97103290b70ac7549151bd28

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -316,10 +316,11 @@ public class PhoneStatusBarView extends PanelBar implements TunerService.Tunable
     }
 
     @Override
-    public void panelScrimMinFractionChanged(float minFraction) {
+    public void onPanelMinFractionChanged(float minFraction) {
         if (isNaN(minFraction)) {
             throw new IllegalArgumentException("minFraction cannot be NaN");
         }
+        super.onPanelMinFractionChanged(minFraction);
         if (mMinFraction != minFraction) {
             mMinFraction = minFraction;
             updateScrimFraction();
