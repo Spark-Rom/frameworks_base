90b574c0ec36deaf545d92be7d6935e2913d8fb3
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    [Squash] Add SystemUI Stub along with smartspace and backgesture provider
    - Squash of following commits :-
    SystemUI: Initial SystemUI stub
    SystemUI: update monet according to new sysui stub
    SystemUI: integrate smartspace and backgesture provider
    
    Change-Id: Icf9a8a38455ae8984446ef6395d282bce51ccf52

---
+++ packages/SystemUI/src/com/spark/android/systemui/dagger/SysUISubcomponentModuleSpark.java
@@ -0,0 +1,10 @@
+package com.spark.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SysUIComponentSpark.class})
+public abstract class SysUISubcomponentModuleSpark {
+}
