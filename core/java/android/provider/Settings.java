dd0c6f48a9fe4a782865c8426cc7227a0522f53c
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    @neobuddy89: Clean up unused code and convert to 1-100 scale.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5887,6 +5887,11 @@ public final class Settings {
 	public static final String KG_SHOW_WEATHER_TEMP = "kg_show_weather_temp";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
