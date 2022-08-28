56ffb5cf0ff28ef36c511a9ce92b6728796b67a5
Pranav Vashi <neobuddy89@gmail.com>
2022-08-28 14:25:01 +0530

    SystemUI: Update and fix QS tile layout settings
    * Do not let every tom, dick and harry set max columns.
    * On config changes and settings update, check if dynamic row and columns update.
    * Update resources in tile layout when orientation changes to get proper tiles in landscape.
    * Request layout changes whenever column update happens.
    * Be smart in calculating how many max tiles should Quick QS show depending on no. of columns.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
@@ -119,12 +119,15 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel>
         mTunerService.addTunable(mView, QSPanel.QS_BRIGHTNESS_SLIDER_POSITION);
         mTunerService.addTunable(mView, QSPanel.QS_SHOW_AUTO_BRIGHTNESS);
         mTunerService.addTunable(mView, QSPanel.QS_SHOW_BRIGHTNESS_SLIDER);
+        mTunerService.addTunable(mView, QSPanel.QS_LAYOUT_COLUMNS);
+        mTunerService.addTunable(mView, QSPanel.QS_LAYOUT_COLUMNS_LANDSCAPE);
 
         mView.setBrightnessRunnable(() -> {
             mView.updateResources();
             updateBrightnessMirror();
         });
 
+        updateConfig();
         mBrightnessMirrorHandler.onQsPanelAttached();
     }
 
@@ -134,6 +137,14 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel>
         mTunerService.removeTunable(mView);
         mView.setBrightnessRunnable(null);
         mBrightnessMirrorHandler.onQsPanelDettached();
+    }
+
+    private void updateConfig() {
+        int maxTiles = getResources().getInteger(R.integer.quick_qs_panel_max_tiles);
+        int columns = getResources().getInteger(R.integer.quick_settings_num_columns);
+        columns = TileUtils.getQSColumnsCount(getContext(), columns);
+        setMaxTiles(Math.max(columns, maxTiles));
+        setTiles();
     }
 
     private void updateBrightnessMirror() {
@@ -159,11 +170,6 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel>
         return mView.isListening();
     }
 
-    private void setMaxTiles(int parseNumTiles) {
-        mView.setMaxTiles(parseNumTiles);
-        setTiles();
-    }
-
     @Override
     public void refreshAllTiles() {
         mBrightnessController.checkRestrictionAndSetEnabled();
@@ -172,11 +178,6 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel>
 
     @Override
     protected void onConfigurationChanged() {
-        int newMaxTiles = getResources().getInteger(R.integer.quick_qs_panel_max_tiles);
-        newMaxTiles = TileUtils.getQSColumnsCount(getContext(), newMaxTiles);
-        if (newMaxTiles != mView.getNumQuickTiles()) {
-            setMaxTiles(newMaxTiles);
-        }
         updateMediaExpansion();
     }
 
