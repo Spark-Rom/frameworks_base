98d98cf64250da4d682df50d5d3c70e191f83e47
jhonboy121 <alfredmathew05@gmail.com>
2022-09-29 21:13:17 +0530

    base: merge hidden app feature with applock
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -387,6 +387,5 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.MONET_ENGINE_CHROMA_FACTOR, ANY_FLOAT_VALIDATOR);
         VALIDATORS.put(Secure.MONET_ENGINE_LINEAR_LIGHTNESS, new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.MONET_ENGINE_WHITE_LUMINANCE, new InclusiveIntegerRangeValidator(0, 1000));
-        VALIDATORS.put(Secure.LAUNCHER_HIDDEN_APPS, APP_LIST_VALIDATOR);
     }
 }
