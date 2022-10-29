1871a0f35c18ad05e480746cd61d3c17d288e141
minaripenguin37 <alexfinhart@gmail.com>
2022-10-29 15:02:12 +0800

    SystemUI: Volume styles [1/2]
    SystemUI: Implement Aqua design to volume panel
    
    * ui concept by AYAN98542
    
    Co-authored-by: Ayan <79016495+AYAN98542@users.noreply.github.com>
    Change-Id: I29dbc7238810e5332465f57fd3d9732c949c756e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6136,6 +6136,12 @@ public final class Settings {
         public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
 
         /**
+         * Volume styles
+         * @hide
+         */
+        public static final String CUSTOM_VOLUME_STYLES = "custom_volume_styles";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
