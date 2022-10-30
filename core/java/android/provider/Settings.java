ca1c5199f57fa8ccde3c784ce8041f58b1fb8f76
minaripenguin37 <alexfinhart@gmail.com>
2022-10-31 05:10:29 +0800

    SystemUI: Custom volume styles [1/2]
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7123,7 +7123,13 @@ public final class Settings {
          * @hide
          */
         public static final String SHOW_APP_VOLUME = "show_app_volume";
-        
+
+        /**
+         * Volume styles
+         * @hide
+         */
+        public static final String CUSTOM_VOLUME_STYLES = "custom_volume_styles";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
