e90f066603b20771d9adc59e12686e15db327339
KrutosVIP <krutosviprus@gmail.com>
2022-11-10 15:42:32 +0400

    base: Make NavigationBar to use Hidden IME Space only on gesture bar
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -1608,7 +1608,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                     com.android.internal.R.bool.config_navBarCanMove);
         }
         if (!navBarCanMove) {
-	    if (IsHideIMESpaceEnabled) {
+	    if (IsHideIMESpaceEnabled && isGesturalMode(mNavBarMode)) {
               height = userContext.getResources().getDimensionPixelSize(
                       com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
             } else {
@@ -1622,7 +1622,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                 case ROTATION_UNDEFINED:
                 case Surface.ROTATION_0:
                 case Surface.ROTATION_180:
-                    if (IsHideIMESpaceEnabled) {
+                    if (IsHideIMESpaceEnabled && isGesturalMode(mNavBarMode)) {
                       height = userContext.getResources().getDimensionPixelSize(
                               com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
                     } else {
