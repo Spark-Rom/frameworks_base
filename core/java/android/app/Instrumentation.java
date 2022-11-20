2fb03e8c133b78535e0771f001afb8594cc35937
Joey Huab <joey@evolution-x.org>
2022-11-20 15:05:24 +0000

    PixelPropsUtils: Merge back AttestationHooks into PixelPropsUtils
    Revert "[SQUASH] Switch to AttestationHooks"
    
    This reverts commit 8525dd56d5d7209babb449bc5669465b9b8ba7d9.
    
    Ref: https://github.com/crdroidandroid/android_frameworks_base/commit/c5ed4b1c5b922a44f55cabb406ce0bf4f0be543e
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -67,7 +67,6 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.spark.PixelPropsUtils;
-import com.android.internal.util.spark.AttestationHooks;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1237,7 +1236,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        AttestationHooks.initApplicationBeforeOnCreate(app);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
         return app;
@@ -1258,7 +1256,6 @@ public class Instrumentation {
         GmsCompat.maybeEnable(context);
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        AttestationHooks.initApplicationBeforeOnCreate(app);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
         return app;
