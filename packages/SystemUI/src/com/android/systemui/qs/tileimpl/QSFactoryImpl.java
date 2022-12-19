c90deb5cf6ba519ecaee3a8eca214c556e484262
ShevT <evgeny.shishkov@gmail.com>
2022-12-19 16:24:06 +0300

    base: Added screenshot tile
    OmniROM:
    
    From: maxwen <max.weninger@gmail.com>
    Date: Wed, 14 Oct 2020 17:17:31 +0200
    Subject: [PATCH] SystemUI: qs tiles revenge
    
    -screenshot
    
    [micky387]
     * edit to the new A12 Tiles API
     * Add secondaryLabel for the long press screenshot tile
    
    From: micky387 <mickaelsaibi@free.fr>
    Date: Wed, 14 Dec 2022 13:12:26 +0100
    Subject: [PATCH] SystemUI: Update ScreenshotTile after merge

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -69,6 +69,7 @@ import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RefreshRateTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -142,6 +143,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SmartPixelsTile> mSmartPixelsTileProvider;
     private final Provider<RefreshRateTile> mRefreshRateTileProvider;
     private final Provider<PreferredNetworkTile> mPreferredNetworkTileProvider;
+    private final Provider<ScreenshotTile> mScreenshotTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -201,7 +203,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SoundSearchTile> soundSearchTileProvider,
             Provider<SmartPixelsTile> smartPixelsTileProvider,
             Provider<RefreshRateTile> refreshRateTileProvider,
-            Provider<PreferredNetworkTile> preferredNetworkTileProvider) {
+            Provider<PreferredNetworkTile> preferredNetworkTileProvider,
+            Provider<ScreenshotTile> screenshotTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -257,6 +260,7 @@ public class QSFactoryImpl implements QSFactory {
         mSmartPixelsTileProvider = smartPixelsTileProvider;
         mRefreshRateTileProvider = refreshRateTileProvider;
         mPreferredNetworkTileProvider = preferredNetworkTileProvider;
+        mScreenshotTileProvider = screenshotTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -376,6 +380,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mRefreshRateTileProvider.get();
             case "preferred_network":
                 return mPreferredNetworkTileProvider.get();
+            case "screenshot":
+                return mScreenshotTileProvider.get();
         }
         // Custom tiles
         if (tileSpec.startsWith(CustomTile.PREFIX)) {
