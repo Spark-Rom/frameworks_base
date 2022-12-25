150b20d8a968f4aac84ccac998981645e81213a5
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-26 07:41:24 +0800

    base: Bring in Weather service API for LsWeather
    This commit does not require any WeatherJaw commits in FWB or adding back weather parts.
    Am Keeping Google's Weather Slice for lockscreen weather.
    
    Special Thanks to @omnirom for the OmniJawsClient and services package
    @AICP for their statusbar weather base code adapted for QS Panel and A11
    Ref: https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/49630
    
    Change-Id: Iafa109a166a6c4106b15c4bff4dfd322718bed84
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Trishiraj <gamerprince.exp@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5869,6 +5869,23 @@ public final class Settings {
         @Readable
         public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
 
+	/**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+	/**
+         * Keyguard weather temperature
+         * 0: Hide the temperature
+         * 1: Display the temperature with scale and image
+         * 2: Display the temperature without scale and with image
+         * 3: Display the temparature with scale and without image
+         * 4: Display the temperature without scale and without image
+         * 5: Display the temperature image
+         * @hide
+         */
+	public static final String KG_SHOW_WEATHER_TEMP = "kg_show_weather_temp";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -5991,6 +6008,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
             PRIVATE_SETTINGS.add(DESKTOP_MODE);
+	    PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
         }
 
         /**
