ec071efdd4f567bef57ba81875b1137a04ea3e39
Ghosuto <clash.raja10@gmail.com>
2022-12-26 15:28:46 +0000

    fix PixelPropsUtils: Netflix Spoof

--- core/java/com/android/internal/util/spark/PixelPropsUtils.java
+++ core/java/com/android/internal/util/spark/PixelPropsUtils.java
@@ -72,7 +72,7 @@ public class PixelPropsUtils {
             "com.android.chrome",
             "com.android.vending",
             "com.breel.wallpapers20",
-            PACKAGE_NETFLIX,
+            "com.netflix.mediaclient",
             "com.disney.disneyplus",
             "com.microsoft.android.smsorganizer",
             "com.nhs.online.nhsonline",
