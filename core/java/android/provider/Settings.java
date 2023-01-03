fd5ba774bc27eca11b9b1529fe26acf16cf5e65d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-03 20:35:13 +0800

    SystemUI: Configurable top margin for ls clock
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12826,6 +12826,11 @@ public final class Settings {
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
         /**
+         * @hide
+         */
+        public static final String KG_CUSTOM_CLOCK_TOP_MARGIN = "kg_custom_clock_top_margin";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
