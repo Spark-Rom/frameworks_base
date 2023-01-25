a2681c7e2d7a1c5d640ec6f2f766c5c76150b524
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-26 05:49:14 +0800

    SystemUI: Lockscreen clock format settings [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7253,6 +7253,12 @@ public final class Settings {
         public static final String SPARK_SYSTEM_BOOST = "spark_system_boost";
 
         /**
+         * Lockscreen clock format
+         * @hide
+         */
+        public static final String CLOCK_USE_CUSTOM_FORMAT = "clock_use_custom_format";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
