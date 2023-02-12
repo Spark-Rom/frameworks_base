d78b3b2476ee36dc92899d7e6ca770dfca1402c5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-12 14:35:59 +0800

    core: Seperate fling velocity configuration for gesture recognition and Views
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -226,6 +226,16 @@ public class ViewConfiguration {
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
     private static final int MAXIMUM_FLING_VELOCITY = 21000;
+    
+    /**
+     * Minimum velocity to initiate a gesture fling, as measured in dips per second
+     */
+    private static final int MINIMUM_GESTURE_FLING_VELOCITY = 50;
+
+    /**
+     * Maximum velocity to initiate a gesture fling, as measured in dips per second
+     */
+    private static final int MAXIMUM_GESTURE_FLING_VELOCITY = 8000;
 
     /**
      * Delay before dispatching a recurring accessibility event in milliseconds.
@@ -327,7 +337,9 @@ public class ViewConfiguration {
     private final int mEdgeSlop;
     private final int mFadingEdgeLength;
     private final int mMinimumFlingVelocity;
+    private final int mMinimumGestureFlingVelocity;
     private final int mMaximumFlingVelocity;
+    private final int mMaximumGestureFlingVelocity;
     private final int mScrollbarSize;
     private final int mTouchSlop;
     private final int mMinScalingSpan;
@@ -371,7 +383,9 @@ public class ViewConfiguration {
         mEdgeSlop = EDGE_SLOP;
         mFadingEdgeLength = FADING_EDGE_LENGTH;
         mMinimumFlingVelocity = MINIMUM_FLING_VELOCITY;
+        mMinimumGestureFlingVelocity = MINIMUM_GESTURE_FLING_VELOCITY;
         mMaximumFlingVelocity = MAXIMUM_FLING_VELOCITY;
+        mMaximumGestureFlingVelocity = MAXIMUM_GESTURE_FLING_VELOCITY;
         mScrollbarSize = SCROLL_BAR_SIZE;
         mTouchSlop = TOUCH_SLOP;
         mHoverSlop = TOUCH_SLOP / 2;
@@ -553,6 +567,11 @@ public class ViewConfiguration {
                 com.android.internal.R.integer.config_smartSelectionInitializingTimeoutMillis);
         mPreferKeepClearForFocusEnabled = res.getBoolean(
                 com.android.internal.R.bool.config_preferKeepClearForFocus);
+                
+        mMinimumGestureFlingVelocity = res.getDimensionPixelSize(
+                com.android.internal.R.dimen.config_viewMinGestureFlingVelocity);
+        mMaximumGestureFlingVelocity = res.getDimensionPixelSize(
+                com.android.internal.R.dimen.config_viewMaxGestureFlingVelocity);
     }
 
     /**
@@ -883,10 +902,25 @@ public class ViewConfiguration {
     }
 
     /**
+     * @return Minimum velocity to initiate a gesture fling, as measured in dips per second.
+     *
+     */
+    public static int getMinimumGestureFlingVelocity() {
+        return MINIMUM_GESTURE_FLING_VELOCITY;
+    }
+
+    /**
      * @return Minimum velocity to initiate a fling, as measured in pixels per second.
      */
     public int getScaledMinimumFlingVelocity() {
         return mMinimumFlingVelocity;
+    }
+
+    /**
+     * @return Minimum velocity to initiate a gesture fling, as measured in pixels per second.
+     */
+    public int getScaledMinimumGestureFlingVelocity() {
+        return mMinimumGestureFlingVelocity;
     }
 
     /**
@@ -900,10 +934,25 @@ public class ViewConfiguration {
     }
 
     /**
+     * @return Maximum velocity to initiate a gesture fling, as measured in dips per second.
+     *
+     */
+    public static int getMaximumGestureFlingVelocity() {
+        return MAXIMUM_GESTURE_FLING_VELOCITY;
+    }
+
+    /**
      * @return Maximum velocity to initiate a fling, as measured in pixels per second.
      */
     public int getScaledMaximumFlingVelocity() {
         return mMaximumFlingVelocity;
+    }
+
+    /**
+     * @return Maximum velocity to initiate a gesture fling, as measured in pixels per second.
+     */
+    public int getScaledMaximumGestureFlingVelocity() {
+        return mMaximumGestureFlingVelocity;
     }
 
     /**
