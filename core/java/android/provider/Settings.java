f25e83edbcdb014860e65108304dde40c0948384
Pranav Vashi <neobuddy89@gmail.com>
2023-04-18 23:24:48 +0530

    base: Add switch to unlink ringtone and notifications volume [1/2]
    * Make use of AOSP config to set default initial value
    
    Ref old LOS implementation:
    https://github.com/crdroidandroid/android_frameworks_base/commit/345ff7b8618bd57f6c6ccbf875cf860338deb513
    https://github.com/crdroidandroid/android_frameworks_base/commit/ab1aad9aa76391d675ca38a93f23120032c1d957
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12118,6 +12118,12 @@ public final class Settings {
         public static final String NAVBAR_LAYOUT_VIEWS = "navbar_layout_views";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Inverse navigation bar layout
          * @hide
          */
