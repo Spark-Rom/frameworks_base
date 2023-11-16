d572beb440f12f83d5af2bb9cb3ca895a8a34a1a
saikiran2001 <bjsaikiran@gmail.com>
2023-11-16 16:53:04 +0000

    PixelPropUtils: Make play integrity pass again

--- core/java/com/android/internal/util/spark/PixelPropsUtils.java
+++ core/java/com/android/internal/util/spark/PixelPropsUtils.java
@@ -232,18 +232,15 @@ public class PixelPropsUtils {
                     Log.e(TAG, "Failed to register task stack listener!", e);
                 }
                 if (was) return true;
-                // Alter build parameters to pixel 2 for avoiding hardware attestation enforcement
-                setPropValue("BRAND", "google");
-                setPropValue("PRODUCT", "walleye");
-                setPropValue("MODEL", "Pixel 2");
-                setPropValue("MANUFACTURER", "Google");
-                setPropValue("DEVICE", "walleye");
-                setPropValue("FINGERPRINT", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-                setPropValue("ID", "OPM1.171019.011");
-                setPropValue("TYPE", "user");
-                setPropValue("TAGS", "release-keys");
-                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.O_MR1);
-                setVersionFieldString("SECURITY_PATCH", "2017-12-05");
+
+                dlog("Spoofing build for GMS");
+                // Alter model name and fingerprint to Redmi Go to avoid hardware attestation enforcement
+                // Alter build parameters to Nexus 6P for avoiding hardware attestation enforcement
+                setPropValue("DEVICE", "bullhead");
+                setPropValue("FINGERPRINT", "google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys");
+                setPropValue("MODEL", "Nexus 5X");
+                setPropValue("PRODUCT", "bullhead");
+                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
                 return true;
             }
         }
